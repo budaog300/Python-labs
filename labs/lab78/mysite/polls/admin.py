e012943d93ba656bcf1826b554a51931ad164f6e
KirillovDanilaIVTASbd-22 <98764032+budaog300@users.noreply.github.com>
2023-06-13 20:58:01 +0400

    Delete labs/lab78 directory

--- labs/lab78/mysite/polls/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Cashier, Store, Check, Sale, Item
-
-admin.site.register(Cashier)
-admin.site.register(Store)
-admin.site.register(Check)
-admin.site.register(Sale)
-admin.site.register(Item)
